4ff1d80258b97c97b397aead1ed66a8e991bbf4b
Aidan Miklos <65783240+aidanMiklos@users.noreply.github.com>
2024-02-22 10:45:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function kursor(removeDefaultCursor) {
   this.removeDefaultCursor = removeDefaultCursor;
-  if(this.removeDefault == True){
+  if (this.removeDefault == true) {
     document.body.classList.add("hideCursor");
   }
 }
